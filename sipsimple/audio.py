536161a2496a538b3a6108a0eb1423794be54143
tarun <agumbe22@gmail.com>
2018-12-13 19:36:41 -0600

    Test

--- sipsimple/audio.py
+++ sipsimple/audio.py
@@ -17,7 +17,7 @@ from eventlib import coros
 from twisted.internet import reactor
 from zope.interface import Attribute, Interface, implements
 
-from sipsimple.core import MixerPort, RecordingWaveFile, SIPCoreError, WaveFile, TTYDemodulator, TTYModulator
+from sipsimple.core import MixerPort, RecordingWaveFile, SIPCoreError, WaveFile, TTYDemodulator, TTYModulator, wave_tty_test
 from sipsimple.threading import run_in_twisted_thread
 from sipsimple.threading.green import Command, run_in_waitable_green_thread
 
@@ -686,7 +686,7 @@ def trace(self, text):
 def wave_tty_tone_test():
     try:
         trace("started wave_tty_tone_test")
-        #wave_tty_test()
+        wave_tty_test()
         trace("finished wave_tty_tone_test")
     except Exception as e:
         trace("wave_tty_tone_test exception {}".format(str(e)))
